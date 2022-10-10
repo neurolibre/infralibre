65b635850ad6655b37d7bbe99201978e9d449c4e
Agah Karakuzu <agahkarakuzu@gmail.com>
2022-10-10 00:45:14 -0400

    Fix is_computecanada count conditions

--- terraform-modules/providers/openstack/main.tf
+++ terraform-modules/providers/openstack/main.tf
@@ -80,7 +80,7 @@ locals {
 }
 
 resource "openstack_networking_subnet_v2" "subnet" {
-  count = 1 - var.is_computecanada
+  count = var.is_computecanada ? 0 : 1
 
   name        = "subnet"
   network_id  = openstack_networking_network_v2.network_1[0].id
@@ -90,7 +90,7 @@ resource "openstack_networking_subnet_v2" "subnet" {
 }
 
 resource "openstack_networking_network_v2" "network_1" {
-  count = 1 - var.is_computecanada
+  count = var.is_computecanada ? 0 : 1
 
   name = local.network_name
 }
@@ -117,14 +117,14 @@ data "openstack_networking_network_v2" "int_network" {
 }
 
 resource "openstack_networking_router_v2" "router_1" {
-  count = 1 - var.is_computecanada
+  count = var.is_computecanada ? 0 : 1
 
   name                = "${var.project_name}-router"
   external_network_id = data.openstack_networking_network_v2.ext_network.id
 }
 
 resource "openstack_networking_router_interface_v2" "router_interface_1" {
-  count = 1 - var.is_computecanada
+  count = var.is_computecanada ? 0 : 1
 
   router_id = openstack_networking_router_v2.router_1[0].id
   subnet_id = openstack_networking_subnet_v2.subnet[0].id
