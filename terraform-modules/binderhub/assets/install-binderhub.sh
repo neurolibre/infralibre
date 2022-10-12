c6ff9241c6573386060902248ae9a37b24877828
Agah Karakuzu <agahkarakuzu@gmail.com>
2022-10-12 03:05:55 -0400

    force helm to use right kubeconfig

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -7,7 +7,7 @@ cd /home/${admin_user}
 
 # node helath monitoring
 sudo helm repo add deliveryhero https://charts.deliveryhero.io/
-sudo helm install deliveryhero/node-problem-detector --generate-name
+sudo helm install deliveryhero/node-problem-detector --generate-name --kubeconfig ~/.kube/config
 
 #Persistent volume
 kubectl create -f pv.yaml
@@ -21,20 +21,21 @@ sudo helm repo update
 sudo helm install cert-manager --namespace cert-manager --version v1.0.3 jetstack/cert-manager --set installCRDs=true \
   --set nodeSelector."node-role\.kubernetes\.io/master=" \
   --set cainjector.nodeSelector."node-role\.kubernetes\.io/master=" \
-  --set webhook.nodeSelector."node-role\.kubernetes\.io/master="
+  --set webhook.nodeSelector."node-role\.kubernetes\.io/master=" \
+  --kubeconfig ~/.kube/config
 #wait until cert-manager is ready
 kubectl wait --namespace cert-manager \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/instance=cert-manager \
   --timeout=300s
 # apply the issuer(s)
-# kubectl create namespace binderhub
+kubectl create namespace binderhub
 # kubectl apply -f staging-binderhub-issuer.yaml
 kubectl apply -f production-binderhub-issuer.yaml
 
 # Binderhub proxy
 sudo helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx/
-sudo helm install binderhub-proxy ingress-nginx/ingress-nginx --namespace=binderhub -f nginx-ingress.yaml
+sudo helm install binderhub-proxy ingress-nginx/ingress-nginx --namespace=binderhub -f nginx-ingress.yaml --kubeconfig ~/.kube/config
 # wait until nginx is ready (https://kubernetes.github.io/ingress-nginx/deploy/)
 kubectl wait --namespace binderhub \
   --for=condition=ready pod \
@@ -52,7 +53,8 @@ sudo helm install binderhub jupyterhub/binderhub --version=${binder_version} \
   --namespace=binderhub -f config.yaml -f secrets.yaml \
   --set-file jupyterhub.singleuser.extraFiles.repo2data.stringData=repo2data.bash \
   --set-file jupyterhub.singleuser.extraFiles.fill_submission_metadata.stringData=fill_submission_metadata.bash \
-  --set-file jupyterhub.singleuser.extraFiles.jb_build.stringData=jb_build.bash
+  --set-file jupyterhub.singleuser.extraFiles.jb_build.stringData=jb_build.bash \
+  --kubeconfig ~/.kube/config
 kubectl wait --namespace binderhub \
   --for=condition=ready pod \
   --selector=release=binderhub \
@@ -64,5 +66,5 @@ sudo helm repo add grafana https://grafana.github.io/helm-charts
 sudo helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 sudo helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
 sudo helm repo update
-sudo helm install grafana-prod grafana/grafana
-sudo helm install prometheus-prod prometheus-community/prometheus
+sudo helm install grafana-prod grafana/grafana --kubeconfig ~/.kube/config
+sudo helm install prometheus-prod prometheus-community/prometheus --kubeconfig ~/.kube/config
