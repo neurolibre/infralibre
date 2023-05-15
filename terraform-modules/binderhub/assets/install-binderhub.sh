ec586c89c8caf7c057660f762d0881f4e4b4e09c
Agah <agahkarakuzu@gmail.com>
2023-05-15 06:27:44 +0400

    Update cert-manager and apis

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -18,7 +18,7 @@ kubectl create namespace cert-manager
 sudo helm repo add jetstack https://charts.jetstack.io
 sudo helm repo update
 # running on master node to avoid issues with webhook not in the k8s network
-sudo helm install cert-manager --namespace cert-manager --version v1.0.3 jetstack/cert-manager --set installCRDs=true \
+sudo helm install cert-manager --namespace cert-manager --version v1.9.0 jetstack/cert-manager --set installCRDs=true \
   --set nodeSelector."node-role\.kubernetes\.io/master=" \
   --set cainjector.nodeSelector."node-role\.kubernetes\.io/master=" \
   --set webhook.nodeSelector."node-role\.kubernetes\.io/master=" \
@@ -68,9 +68,9 @@ kubectl wait --namespace binderhub \
 
 # Grafana and prometheus
 # https://github.com/pangeo-data/pangeo-binder#binder-monitoring
-sudo helm repo add grafana https://grafana.github.io/helm-charts
-sudo helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-sudo helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
-sudo helm repo update
-sudo helm install grafana-prod grafana/grafana --kubeconfig ~/.kube/config
-sudo helm install prometheus-prod prometheus-community/prometheus --kubeconfig ~/.kube/config
+# sudo helm repo add grafana https://grafana.github.io/helm-charts
+# sudo helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# sudo helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
+# sudo helm repo update
+# sudo helm install grafana-prod grafana/grafana --kubeconfig ~/.kube/config
+# sudo helm install prometheus-prod prometheus-community/prometheus --kubeconfig ~/.kube/config
