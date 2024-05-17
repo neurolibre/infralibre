111b74e030f7537a93e47caf070c82599ed9a047
Agah <agahkarakuzu@gmail.com>
2024-05-17 18:52:59 +0100

    Not allowed

--- terraform-modules/providers/openstack/main.tf
+++ terraform-modules/providers/openstack/main.tf
@@ -215,7 +215,7 @@ resource "openstack_compute_instance_v2" "master" {
 }
 
 resource "openstack_blockstorage_volume_v1" "nodevolume" {
-  name        = "node${count.index + 1}-volume"
+  name        = "node-volume"
   size        = var.instance_volume_size
   image_id    = data.openstack_images_image_v2.ubuntu.id
 }
