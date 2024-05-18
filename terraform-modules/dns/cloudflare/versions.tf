ad422b3a1144da29cc2486f0720b697c97d6d75e
Agah <agahkarakuzu@gmail.com>
2024-05-18 22:50:18 +0100

    EVEN TAYLOR SWIFT IS OK WITH BLANK SPACES

--- terraform-modules/dns/cloudflare/versions.tf
+++ terraform-modules/dns/cloudflare/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     cloudflare = {
-      source = " cloudflare/cloudflare"
+      source = "cloudflare/cloudflare"
       version = "<= 2.10.1"
     }
   }
