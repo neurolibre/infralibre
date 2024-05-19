796d71b573f13437a8448e4f788aa6895d0e31dd
Agah <agahkarakuzu@gmail.com>
2024-05-19 18:31:41 +0100

    Give idx id

--- terraform-modules/providers/openstack/main.tf
+++ terraform-modules/providers/openstack/main.tf
@@ -165,7 +165,7 @@ resource "openstack_compute_instance_v2" "node" {
   )
 
   block_device {
-    uuid                  = openstack_blockstorage_volume_v3.nodevolume.id
+    uuid                  = openstack_blockstorage_volume_v3.nodevolume[count.index].id
     source_type           = "volume"
     destination_type      = "volume"
     boot_index            = 0
