7ba7144b9c9771af91c37d4a8b6b79d9066c507b
Agah <agahkarakuzu@gmail.com>
2024-05-19 16:17:08 +0100

    api..

--- terraform-modules/providers/openstack/main.tf
+++ terraform-modules/providers/openstack/main.tf
@@ -176,7 +176,7 @@ resource "openstack_networking_floatingip_v2" "fip_1" {
   pool = data.openstack_networking_network_v2.ext_network.name
 }
 
-resource "openstack_compute_floatingip_associate_v2" "fip_1" {
+resource "openstack_networking_floatingip_associate_v2" "fip_1" {
   floating_ip = openstack_networking_floatingip_v2.fip_1.address
   port_id     = openstack_networking_port_v2.ext_port.id
 }
