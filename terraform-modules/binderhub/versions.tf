701a58864d97640e08dacbeac4f0ce51282b61d7
Agah <agahkarakuzu@gmail.com>
2024-09-19 22:29:40 -0400

    Add cfl

--- terraform-modules/binderhub/versions.tf
+++ terraform-modules/binderhub/versions.tf
@@ -2,6 +2,10 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 4.40.0"
+    }
     null = {
       source = "hashicorp/null"
     }
