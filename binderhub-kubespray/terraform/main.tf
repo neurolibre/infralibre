0ee4c4cea70843d3a05f16fb98a01ad0e35c7d9c
Agah <agahkarakuzu@gmail.com>
2025-04-19 13:18:08 -0400

    My bad

--- binderhub-kubespray/terraform/main.tf
+++ binderhub-kubespray/terraform/main.tf
@@ -106,7 +106,7 @@ resource "terraform_data" "deploy_kubernetes" {
   ]
 
   provisioner "local-exec" {
-    command = "bash ${path.module}/../deploy-kubernetes.sh"
+    command = "bash ${path.module}/../scripts/deploy-kubernetes.sh"
   }
   
 }
