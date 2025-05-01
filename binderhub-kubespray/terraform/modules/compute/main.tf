356ba3c8b299213acad0b9113d8f18bee2d2d91f
Agah <agahkarakuzu@gmail.com>
2025-04-30 22:59:13 -0400

    nailed it

--- binderhub-kubespray/terraform/modules/compute/main.tf
+++ binderhub-kubespray/terraform/modules/compute/main.tf
@@ -33,7 +33,10 @@ data "template_file" "cloud_init_cluster" {
     ceph_rule_key = var.ceph_rule_key
     ceph_share_hash = var.ceph_share_hash
     admin_user = var.admin_user
-    etcd_volume_device = "/dev/disk/by-uuid/${openstack_blockstorage_volume_v3.etcd_volume.id}"
+    # ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+    # HARDCODED CONVENTION FOR OPENSTACK (virtio-${volume_id}(1:20))
+    # -------------------------------------------------------------
+    etcd_volume_device = "/dev/disk/by-id/virtio-${substr(openstack_blockstorage_volume_v3.etcd_volume.id, 0, 20)}"
     cluster_name = var.cluster_name
     shared_data_directory = var.shared_data_directory
   }
@@ -147,7 +150,10 @@ resource "terraform_data" "wait_for_cloud_init_and_mount" {
       "echo '🧿 ..... Mounting volumes .....'",
       "sudo mount -av",
       "sudo chown -R ${var.admin_user}:${var.admin_user} /${var.shared_data_directory} && chmod 755 /${var.shared_data_directory}",
-      "if [ $(hostname) = \"${var.cluster_name}-master\" ]; then sudo mkfs.ext4 /dev/disk/by-uuid/${openstack_blockstorage_volume_v3.etcd_volume.id}; fi",
+      # ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+      # HARDCODED CONVENTION FOR OPENSTACK (virtio-${volume_id}(1:20))
+      # -------------------------------------------------------------
+      "if [ $(hostname) = \"${var.cluster_name}-master\" ]; then sudo mkfs.ext4 /dev/disk/by-id/virtio-${substr(openstack_blockstorage_volume_v3.etcd_volume.id, 0, 20)}; fi",
       "if [ $(hostname) = \"${var.cluster_name}-master\" ]; then sudo chmod 700 /var/lib/etcd; fi",
       "echo '✅ Mount completed successfully on ${count.index}'",
     ]
